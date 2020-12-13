22de120a6ad9f8ecd49e2b1efab919633bf0ef34
jooanc <50957963+jooanc@users.noreply.github.com>
2020-12-13 00:07:13 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("keydown", (event)=>{
+    if(event.code == "KeyA"){
+        let audioA = new Audio("media/A.mp3")
+        audioA.play();
+        console.log("The key 'A' is pressed");
+    } if(event.code == "KeyS"){
+        let audioS = new Audio("media/S.mp3")
+        audioS.play();
+        console.log("The key 'S' is pressed");
+    } if(event.code == "KeyD"){
+        let audioD = new Audio("media/D.mp3")
+        audioD.play();
+        console.log("The key 'D' is pressed");
+    } if(event.code == "KeyF"){
+        let audioF = new Audio("media/F.mp3")
+        audioF.play();
+        console.log("The key 'F' is pressed");
+    } if(event.code == "KeyG"){
+        let audioG = new Audio("media/G.mp3")
+        audioG.play();
+        console.log("The key 'G' is pressed");
+    } if(event.code == "KeyH"){
+        let audioH = new Audio("media/H.mp3")
+        audioH.play();
+        console.log("The key 'H' is pressed");
+    } if(event.code == "KeyJ"){
+        let audioJ = new Audio("media/J.mp3")
+        audioJ.play();
+        console.log("The key 'J' is pressed");
+    } if(event.code == "KeyW"){
+        let audioD = new Audio("media/W.mp3")
+        audioD.play();
+        console.log("The key 'W' is pressed");
+    } if(event.code == "KeyE"){
+        let audioF = new Audio("media/E.mp3")
+        audioF.play();
+        console.log("The key 'E' is pressed");
+    } if(event.code == "KeyT"){
+        let audioG = new Audio("media/T.mp3")
+        audioG.play();
+        console.log("The key 'T' is pressed");
+    } if(event.code == "KeyY"){
+        let audioH = new Audio("media/Y.mp3")
+        audioH.play();
+        console.log("The key 'Y' is pressed");
+    } if(event.code == "KeyU"){
+        let audioJ = new Audio("media/U.mp3")
+        audioJ.play();
+        console.log("The key 'U' is pressed");
+    }
+})
